8ad8200d9f607f72c8d7168a90d7a27a87342554
shaun-sweet <shaun@shaunsweet.com>
2017-06-26 08:26:00 -0700

    Added separation of image areas for easier formatting

--- src/components/ImageOutputArea.js
+++ src/components/ImageOutputArea.js
@@ -6,9 +6,32 @@ import endPointUrl from 'environment';
 const ImageOutputArea = (props) => {
   return (
     <div className="imageOutputArea">
-      <img src={props.src ? endPointUrl+props.src : null} alt=""/>
+      <Mask
+        src={props.src}
+      />
+      <MaskedImage
+
+      />
     </div>
   )
 }
 
 export default ImageOutputArea;
+
+
+const Mask = (props) => {
+  return (
+        <div className="mask">
+          <img src={props.src ? endPointUrl+props.src : null} alt=""/>
+        </div>
+      )
+}
+
+const MaskedImage = (props) => {
+  return (
+    <div className="masked-image">
+
+    </div>
+  )
+
+}
