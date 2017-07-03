42aab7b50f784f45b694f5f7725ad5e52a419ce8
Shaun Sweet <shaun@shaunsweet.com>
2017-07-03 14:32:13 -0700

    Added Slider Titles
    Added slider titles, still need to be styled

--- src/components/SliderControls.js
+++ src/components/SliderControls.js
@@ -7,9 +7,17 @@ import 'styles/sliderControls.css';
 //     sliderValues: [0, 255, 0, 255, 0, 255]
 //   }
 // ]
-const sliders = ["c1min", "c1max", "c2min", "c2max", "c3min", "c3max"];
+const sliders = [
+  {id: "c1min", label: "Ch 1 Min"},
+  {id: "c1max", label: "Ch 1 Max"},
+  {id: "c2min", label: "Ch 2 Min"},
+  {id: "c2max", label: "Ch 2 Max"},
+  {id: "c3min", label: "Ch 3 Min"},
+  {id: "c3max", label: "Ch 3 Max"}
+];
 const Slider = (props) => {
   return <input
+    style={{flex: '1'}}
     type='range'
     className="slider"
     name={props.name}
@@ -28,21 +36,24 @@ const SliderControls = (props) => {
     <div className='slider-controls'>
       {sliders.map((slider) => {
         return(
-          <div className="slider-container" key={slider}>
+          <div className="slider-container" key={slider.id}>
+            <div className="slider-titles">
+              {slider.label}
+            </div>
             <Slider
               type='range'
               className="slider"
-              name={slider}
-              onChange={(e) => props.onChange(e,slider)}
-              id={slider}
+              name={slider.id}
+              onChange={(e) => props.onChange(e,slider.id)}
+              id={slider.id}
               step={props.step}
               min={props.min}
-              value={props.formState[slider]}
+              value={props.formState[slider.id]}
               max={props.max}
             />
             <SliderValueDisplay
-              onChange={(e) => props.onChange(e,slider)}
-              value={props.formState[slider]}
+              onChange={(e) => props.onChange(e,slider.id)}
+              value={props.formState[slider.id]}
             />
           </div>)
       })}
