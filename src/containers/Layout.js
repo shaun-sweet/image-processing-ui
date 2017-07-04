4a9ebe0bcdb29581818297e67cfc0d76377c5190
Shaun Sweet <shaun@shaunsweet.com>
2017-07-03 17:04:28 -0700

    Adding dropzone

--- src/containers/Layout.js
+++ src/containers/Layout.js
@@ -4,6 +4,7 @@ import ImageOutputArea from 'components/ImageOutputArea';
 import ColorspaceLabels from 'components/ColorspaceLabels';
 import RenderButton from 'components/RenderButton';
 import UploadFileButton from 'components/UploadFileButton';
+import Dropzone from 'react-dropzone'
 import endPointUrl from 'config/environment';
 
 import 'styles/Layout.css';
@@ -13,6 +14,9 @@ export default class Layout extends Component {
   constructor() {
     super();
     this.state = {
+      accept: '',
+      files: [],
+      dropzoneActive: false,
       hasBeenRendered: false,
       formData: {
         "c1min": 0,
@@ -25,6 +29,32 @@ export default class Layout extends Component {
     };
     this._handleSliderChange = this._handleSliderChange.bind(this);
     this._handleImageUpload = this._handleImageUpload.bind(this);
+    this.onDrop = this.onDrop.bind(this);
+  }
+
+  onDragEnter() {
+    this.setState({
+      dropzoneActive: true
+    });
+  }
+
+  onDragLeave() {
+    this.setState({
+      dropzoneActive: false
+    });
+  }
+
+  onDrop(files) {
+    this.setState({
+      files,
+      dropzoneActive: false
+    });
+  }
+
+  applyMimeTypes(event) {
+    this.setState({
+      accept: event.target.value
+    });
   }
 
   _handleSliderChange(event, slider) {
@@ -55,31 +85,62 @@ export default class Layout extends Component {
   }
 
   render() {
+    const { accept, files, dropzoneActive } = this.state;
+    const overlayStyle = {
+      position: 'absolute',
+      top: 0,
+      right: 0,
+      bottom: 0,
+      left: 0,
+      padding: '2.5em 0',
+      background: 'rgba(0,0,0,0.5)',
+      textAlign: 'center',
+      color: '#fff'
+    };
     return (
-      <div className="layout">
-        <h3>Colorspace Filter</h3>
-        <form ref='uploadForm'
-          onSubmit={this._handleImageUpload}
-          style={{display: 'flex',
-          flexFlow: 'column'}}
-          id='uploadForm'
-          method='post'
-          encType="multipart/form-data"
-        >
-          <ColorspaceLabels onClick={this.trackFirstRender.bind(this)} hasBeenRendered={this.state.hasBeenRendered} />
-          <SliderControls
-            min="0"
-            max="100"
-            onChange={this._handleSliderChange}
-            formState={this.state.formData}
+      <Dropzone
+        disableClick
+        style={{}}
+        accept={accept}
+        multiple={false}
+        onDrop={this.onDrop.bind(this)}
+        onDragEnter={this.onDragEnter.bind(this)}
+        onDragLeave={this.onDragLeave.bind(this)}
+      >
+        <div className="layout">
+          <h3>Colorspace Filter</h3>
+          <form ref='uploadForm'
+            onSubmit={this._handleImageUpload}
+            style={{display: 'flex',
+            flexFlow: 'column'}}
+            id='uploadForm'
+            method='post'
+            encType="multipart/form-data"
+          >
+            <ColorspaceLabels onClick={this.trackFirstRender.bind(this)} hasBeenRendered={this.state.hasBeenRendered} />
+            <SliderControls
+              min="0"
+              max="100"
+              onChange={this._handleSliderChange}
+              formState={this.state.formData}
+            />
+            <UploadFileButton />
+            { dropzoneActive && <div style={overlayStyle}>Drop files...</div> }
+            <div>
+              <h2>Dropped files</h2>
+              <ul>
+                {
+                  files.map(f => <li>{f.name} - {f.size} bytes</li>)
+                }
+              </ul>
+            </div>
+            <RenderButton />
+          </form>
+          <ImageOutputArea
+            src={this.state.imgUrl}
           />
-          <UploadFileButton />
-          <RenderButton />
-        </form>
-        <ImageOutputArea
-          src={this.state.imgUrl}
-        />
-      </div>
+        </div>
+      </Dropzone>
     );
   }
 }
