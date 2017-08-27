f567872dcc08d5017f4b029d9f144ce896fdcb07
Shaun Sweet <shaun@shaunsweet.com>
2017-08-27 16:10:57 -0700

    Tabs and image styling

--- src/components/ImageOutputArea.js
+++ src/components/ImageOutputArea.js
@@ -7,37 +7,54 @@ export default class ImageOutputArea extends Component {
   constructor() {
     super();
     this.state = {
-      hello: 'world'
-    }
+      activeTab: 'mask',
+    };
+
+    this._handleTabClick = this._handleTabClick.bind(this);
   }
+
+  _handleTabClick(tab) {
+    this.setState({ activeTab: tab });
+  }
+
+  _isActive(tab) {
+    return tab == this.state.activeTab;
+  }
+
   render () {
     return (
       <div className="imageOutputArea">
+        <div className="image-output-tab">
+          <span onClick={() => this._handleTabClick('mask')} className="mask-tab">Mask</span>
+          <span onClick={() => this._handleTabClick('maskApplied')} className="mask-applied">Mask Applied</span>
+        </div>
         <Mask
+          show={this._isActive('mask')}
           src={this.props.images.mask}
         />
         <MaskedImage
           src={this.props.images.masked}
+          show={this._isActive('maskApplied')}
         />
-    </div>)
+      </div>
+    )
   }
 }
 
 const Mask = (props) => {
+  if (!props.show) return null;
   return (
     <div className="mask">
-      {props.src ? <h6>Mask</h6> : null}
       <img src={props.src ? endPointUrl+props.src : null} alt=""/>
     </div>
   )
 }
 
 const MaskedImage = (props) => {
+  if (!props.show) return null;
   return (
     <div className="masked-image">
       <img src={props.src ? endPointUrl+props.src : null} alt=""/>
-      {props.src ? <h6>Mask Applied</h6> : null}
-        </div>
-        )
-
-        }
+    </div>
+  )
+}
